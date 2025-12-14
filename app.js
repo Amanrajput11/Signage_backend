16b4f318be52a767451a4d326df9430d246ec7d8
Amanrajput11 <125857344+Amanrajput11@users.noreply.github.com>
2025-12-14 16:44:56 +0530

    upload path

--- app.js
+++ app.js
@@ -94,7 +94,7 @@ app.use(cors(corsOptions));
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 // Routes
 app.use("/api/media", mediaRoutes);
 
