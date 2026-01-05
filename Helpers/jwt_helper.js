1300b48e3546bb2bf7fc8b5ab5b1a154b03e6a74
Omkar-Gawali <omkargawali702@gmail.com>
2026-01-05 20:51:56 +0530

    undo the changes

--- Helpers/jwt_helper.js
+++ Helpers/jwt_helper.js
@@ -24,45 +24,32 @@ module.exports = {
       });
     });
   },
-  verifyAccessToken: async (req, res, next) => {
-    try {
-      if (!req.headers.authorization) {
-        return next(createError.Unauthorized("No authorization header"));
-      }
 
-      const authHeader = req.headers.authorization;
-      const token = authHeader.split(" ")[1];
-
-      if (!token) {
-        return next(createError.Unauthorized("Unauthorized"));
-      }
-
-      JWT.verify(
-        token,
-        process.env.ACCESS_TOKEN_SECRET,
-        async (err, payload) => {
-          if (err) {
-            return next(
-              createError.Unauthorized(
-                err.name === "JsonWebTokenError" ? "Unauthorized" : err.message
-              )
-            );
-          }
-
-          const user = await User.findById(payload.aud);
-          if (!user) {
-            return next(createError.Unauthorized("User not found"));
-          }
-
-          req.user = user;
-          req.payload = payload;
-          next();
-        }
-      );
-    } catch (err) {
-      next(err);
+  verifyAccessToken: (req, res, next) => {
+    if (!req.headers.authorization) return next(createError.Unauthorized());
+    const authHeader = req.headers.authorization;
+    const bearerToken = authHeader.split(" ");
+    const token = bearerToken[1];
+    console.log("Access Token:", token);
+    // const token = authHeader;
+    if (!token) {
+      createError.Unauthorized("Unauthorized");
     }
+    JWT.verify(token, process.env.ACCESS_TOKEN_SECRET, async (err, payload) => {
+      if (err) {
+        const message =
+          err.name === "JsonWebTokenError" ? "Unauthorized" : err.message;
+        return next(createError.Unauthorized(message));
+      }
+      const user = await User.findOne({
+        _id: new mongoose.Types.ObjectId(payload.aud),
+      });
+      req.user = user;
+      req.payload = payload;
+      next();
+    });
   },
+
   signRefreshToken: (userId) => {
     return new Promise((resolve, reject) => {
       const payload = {};
